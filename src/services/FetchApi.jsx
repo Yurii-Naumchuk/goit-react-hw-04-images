59f278bc151a1c06c042a9da426a62c5d351f17f
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-11-15 00:45:39 +0200

    fix

--- src/services/FetchApi.jsx
+++ src/services/FetchApi.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '29397538-d4347fe79bd92696eb0b247a2';
 
 const fetchRequest = async (searchName, page) => {
 
-  const URL = `${BASE_URL}?q=${searchName}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12&`;
+  const URL = `?q=${searchName}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12&`;
   const response = await axios.get(URL);
   return response.data;
 };
